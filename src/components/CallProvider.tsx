d1969ad6ce026fa20f45b44216e8a718683b7fe5
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2023-01-30 11:13:21 +0100

    Add notifications

--- src/components/CallProvider.tsx
+++ src/components/CallProvider.tsx
@@ -1,35 +1,50 @@
-import type { HMSStore, IStoreReadOnly } from "@100mslive/hms-video-store";
+import type {
+  HMSStatsStore,
+  HMSStore,
+  IHMSNotifications,
+  IStoreReadOnly,
+} from "@100mslive/hms-video-store";
 import { HMSReactiveStore } from "@100mslive/hms-video-store";
 import type { IHMSActions } from "@100mslive/hms-video-store/dist/core/IHMSActions";
 import React, { useEffect, useMemo } from "react";
 
 interface CallContextValue {
-  hmsStore: IStoreReadOnly<HMSStore> | null;
-  hmsActions: IHMSActions | null;
+  store: IStoreReadOnly<HMSStore> | null;
+  actions: IHMSActions | null;
+  notifications: IHMSNotifications | null;
+  statsStore?: HMSStatsStore;
 }
 
 export const CallContext = React.createContext<CallContextValue>({
-  hmsStore: null,
-  hmsActions: null,
+  store: null,
+  actions: null,
+  notifications: null,
 });
 
 interface CallProviderProps {
+  /**
+   * Enable stats. Unused for the moment
+   */
+  isHMSStatsOn?: boolean;
   children?: React.ReactNode | React.ReactNode[];
 }
 
-export const CallProvider: React.FC<CallProviderProps> = ({ children }) => {
+export const CallProvider: React.FC<CallProviderProps> = ({
+  children,
+  isHMSStatsOn: _ = false,
+}) => {
   const hmsManager = useMemo<HMSReactiveStore>(
     () => new HMSReactiveStore(),
     []
   );
 
-  const value = useMemo<CallContextValue>(
-    () => ({
-      hmsStore: hmsManager.getStore(),
-      hmsActions: hmsManager.getActions(),
-    }),
-    [hmsManager]
-  );
+  const value = useMemo<CallContextValue>(() => {
+    return {
+      store: hmsManager.getStore(),
+      actions: hmsManager.getActions(),
+      notifications: hmsManager.getNotifications(),
+    };
+  }, [hmsManager]);
 
   useEffect(() => {
     hmsManager.triggerOnSubscribe();
--- src/hooks/useCall.ts
+++ src/hooks/useCall.ts
@@ -1,9 +1,15 @@
 import { useContext, useState, useEffect, useRef } from "react";
-import type { HMSStore, IStoreReadOnly } from "@100mslive/hms-video-store";
+import type {
+  HMSNotificationInCallback,
+  HMSNotificationTypeParam,
+  HMSStore,
+  IStoreReadOnly,
+} from "@100mslive/hms-video-store";
 import { selectVideoTrackByID } from "@100mslive/hms-video-store";
 import type { IHMSActions } from "@100mslive/hms-video-store/dist/core/IHMSActions";
 import { CallContext } from "../components/CallProvider";
-import type { StateSelector } from "zustand";
+import type { StateSelector, EqualityChecker } from "zustand";
+import shallow from "zustand/shallow";
 
 export const useHMSStore = () => {
   const context = useContext(CallContext);
@@ -12,7 +18,7 @@ export const useHMSStore = () => {
       "useHMSStore can only be used in descendants of CallProvider"
     );
   }
-  return context.hmsStore as IStoreReadOnly<HMSStore>;
+  return context.store as IStoreReadOnly<HMSStore>;
 };
 
 /**
@@ -21,17 +27,21 @@ export const useHMSStore = () => {
  * @returns T
  */
 export const useHMSState = <T = unknown>(
-  selector: StateSelector<HMSStore, T>
+  selector: StateSelector<HMSStore, T>,
+  equalityFn: EqualityChecker<T> = shallow
 ) => {
   const store = useHMSStore();
   const [value, setValue] = useState<T>();
   useEffect(() => {
-    store.subscribe((v) => setValue(v), selector);
-  }, [selector, store]);
+    store.subscribe((v) => setValue(v), selector, equalityFn);
+  }, [equalityFn, selector, store]);
 
   return value;
 };
 
+/*
+ * `useHMSActions` is a write ony hook which can be used to dispatch actions.
+ */
 export const useHMSActions = () => {
   const context = useContext(CallContext);
   if (!context) {
@@ -39,7 +49,34 @@ export const useHMSActions = () => {
       "useHMSActions can only be used in descendants of CallProvider"
     );
   }
-  return context.hmsActions as IHMSActions;
+  return context.actions as IHMSActions;
+};
+
+/**
+ * `useHMSNotifications` is a read only hook which gives the latest notification(HMSNotification) received.
+ */
+export const useHMSNotifications = <T extends HMSNotificationTypeParam>() => {
+  const context = useContext(CallContext);
+  const [notification, setNotification] =
+    useState<HMSNotificationInCallback<T> | null>(null);
+
+  if (!context) {
+    throw new Error(
+      "useHMSNotifications can only be used in descendants of CallProvider"
+    );
+  }
+
+  useEffect(() => {
+    if (!context.notifications) {
+      return;
+    }
+    const unsubscribe = context.notifications.onNotification<T>(
+      (notification) => setNotification(notification)
+    );
+    return unsubscribe;
+  }, [context.notifications]);
+
+  return notification;
 };
 
 export const useVideo = (trackId: string) => {
