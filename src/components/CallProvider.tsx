890bd2f7944173d3cf54b108bdfdc86fb22df9e1
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2023-01-30 10:35:18 +0100

    Better memoisation

--- src/components/CallProvider.tsx
+++ src/components/CallProvider.tsx
@@ -3,10 +3,12 @@ import { HMSReactiveStore } from "@100mslive/hms-video-store";
 import type { IHMSActions } from "@100mslive/hms-video-store/dist/core/IHMSActions";
 import React, { useEffect, useMemo } from "react";
 
-export const CallContext = React.createContext<{
+interface CallContextValue {
   hmsStore: IStoreReadOnly<HMSStore> | null;
   hmsActions: IHMSActions | null;
-}>({
+}
+
+export const CallContext = React.createContext<CallContextValue>({
   hmsStore: null,
   hmsActions: null,
 });
@@ -16,22 +18,22 @@ interface CallProviderProps {
 }
 
 export const CallProvider: React.FC<CallProviderProps> = ({ children }) => {
-  const hmsManager = useMemo(() => {
-    return new HMSReactiveStore();
-  }, []);
+  const hmsManager = useMemo<HMSReactiveStore>(
+    () => new HMSReactiveStore(),
+    []
+  );
+
+  const value = useMemo<CallContextValue>(
+    () => ({
+      hmsStore: hmsManager.getStore(),
+      hmsActions: hmsManager.getActions(),
+    }),
+    [hmsManager]
+  );
 
   useEffect(() => {
     hmsManager.triggerOnSubscribe();
   }, [hmsManager]);
 
-  return (
-    <CallContext.Provider
-      value={{
-        hmsStore: hmsManager.getStore(),
-        hmsActions: hmsManager.getActions(),
-      }}
-    >
-      {children}
-    </CallContext.Provider>
-  );
+  return <CallContext.Provider value={value}>{children}</CallContext.Provider>;
 };
